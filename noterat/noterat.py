55d88be0f2c5aaf2e5648468baa2898277f1d537
Kokodod <daniel.moll@outlook.com>
2025-03-27 21:08:43 +0100

    feat(): define commands

--- noterat/noterat.py
+++ noterat/noterat.py
@@ -1,16 +1,20 @@
 import click
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 def add():
     click.echo("[+] Note added!")
 
+
 @cli.command()
 def delete():
     click.echo("[-] Note deleted!")
+
 
 @cli.command()
 def search():
